f02bb55c4e23e086b3f4ae959b866c9ac2c6ece5
Mohamed Salman <mohamed.akj@gmail.com>
2023-05-10 11:58:12 +0530

    Added fileupload APIs

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Union, Annotated
 
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, File, UploadFile
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -11,11 +12,23 @@ class Item(BaseModel):
     is_offer: Union[bool, None] = None
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def main():
+    content = """
+    <body>
+        <form action="/files/" enctype="multipart/form-data" method="post">
+            <input name="files" type="file" multiple>
+            <input type="submit">
+        </form>
+        <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+            <input name="files" type="file" multiple>
+            <input type="submit">
+        </form>
+    </body>
+    """
+    return HTMLResponse(content=content)
 
 @app.get("/items/{item_id}")
-def read_item(
+async def read_item(
     item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)], 
     q: Annotated[str | None, Query(max_length=50)] = None,
 ):
@@ -29,5 +42,13 @@ async def create_item(item: Item):
     return item
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.post("/files/")
+async def create_files(files: Annotated[list[bytes], File()]):
+    return {"file_size": [len(file) for file in files]}
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: list[UploadFile]):
+    return {"filename": [file.filename for file in files]}
